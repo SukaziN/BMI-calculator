1818881d3bf8cd01b78f0e7d8da56774e9d55153
SukaziN <nkosiyazisukazie@gmail.com>
2026-01-16 13:00:08 +0200

    corrected BMI calculation

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ console.log(inches);
 let pounds = document.getElementById("weight").value;
 pounds = Number(pounds);
 console.log(pounds);
-let centimeters = (inches * 2.54) ** 2; //centimeter conversion is in brackets. The additional square is a modification adjusted for the meters
+let centimeters = inches * 2.54;
 console.log(centimeters);
 let meters = centimeters / 100;
 console.log(meters);
 let kilograms = pounds / 2.2046;
 console.log(kilograms);
 
-let BMI = kilograms / meters; //this returns a ridiculously large number
+let BMI = kilograms / meters ** 2; //this returns a ridiculously large number
 //console.log(meters ** 2);
 console.log(BMI);
-//I ended up squaring the centimeters before the meters conversion because the meters were decimals and the square meters made it a much smaller number which made the BMI division produce a ridiculously large number
